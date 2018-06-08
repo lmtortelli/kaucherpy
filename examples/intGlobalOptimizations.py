21906bf11c118b55f0f67fdb8e20511e66aa498b
Lucas Mendes Tortelli <lmtortelli@hotmail.com>
2018-06-08 19:18:10 -0300

    Correções para server

--- examples/intGlobalOptimizations.py
+++ examples/intGlobalOptimizations.py
@@ -1,53 +1,5 @@
 #!/usr/bin/env python
-""" some n-dimensional test functions for optimization in Python.
-    Example:
-    import numpy as np
-    from ... import ndtestfuncs  # this file, ndtestfuncs.py
-    funcnames = "ackley ... zakharov"
-    dim = 8
-    x0 = e.g. np.zeros(dim)
-    for func in ndtestfuncs.getfuncs( funcnames ):
-        fmin, xmin = myoptimizer( func, x0 ... )
-        # calls func( x ) with x a 1d numpy array or array-like of any size >= 2
-These are the n-dim Matlab functions by A. Hedar (2005), translated to Python-numpy.
-http://www-optima.amp.i.kyoto-u.ac.jp/member/student/hedar/Hedar_files/TestGO.htm
-    ackley.m dp.m griew.m levy.m mich.m perm.m powell.m power.m
-    rast.m rosen.m schw.m sphere.m sum2.m trid.m zakh.m
-    + ellipse nesterov powellsincos randomquad logsumexp
---------------------------------------------------------------------------------
-    All functions appearing in this work are fictitious;
-    any resemblance to real-world functions, living or dead, is purely coincidental.
---------------------------------------------------------------------------------
-Notes
------
-Each `func( x )` works for `x` of any size >= 2.
-Each starts off
-    x = np.asarray_chkfinite(x)  # ValueError if any NaN or Inf
-The values of most functions increase as O(dim) or O(dim^2),
-so the convergence curves for dim 5 10 20 ... are not comparable,
-and `ftol_abs` depends on func() scaling.
-Better would be to scale function values to min 1, max 100 in all dimensions.
-Similarly, `xtol_abs` depends on `x` scaling;
-`x` should be scaled to -1 .. 1 in all dimensions.
-(`ftol_rel` and `xtol_rel` can misbehave near 0; see `isclose`, abs or rel.)
-Results from any optimizer depend of course on `ftol_abs xtol_abs maxeval ...`
-plus hidden or derived parameters, e.g. BOBYQA rho.
-Methods like Nelder-Mead that track sets of points, starting with `x0 + initstep I`,
-are sensitive to `initstep`. And what are `ftol` and `xtol` for sets ?
-Some functions have many local minima or saddle points (more in higher dimensions ?),
-making the final fmin very sensitive to the starting x0.
-Also, some have a local minimum at [0 0 ...] so starting there is boring.
-*Always* look at a few points near a purported xmin.
-Fun constrained problems: min f(x) over the surface of f's bounding box.
-See also
---------
-http://en.wikipedia.org/wiki/Test_functions_for_optimization  -- 2d plots
-http://www.scipy.org/NumPy_for_Matlab_Users
-nlopt/test/... runs and plots BOBYQA PRAXIS SBPLX ... on these ndtestfuncs
-    from several random startpoints, in several dimensions
-Nd-testfuncs-python.md
-F = Funcmon(func): wrap func() to monitor and plot F.fmem F.xmem F.cost
-"""
+
     # zillions of papers and methods for derivative-free optimization alone
 
 
@@ -72,11 +24,12 @@ import math
 #__version__ = "2015-03-06 mar  denis-bz-py t-online de"  # + randomquad logsumexp
 
 def degToRad(value):
-    rad = (value/180)*math.pi
+    rad = (IReal(value.inf/180.0,value.sup/180.0))*math.pi
     if (type(rad) is IReal):
         return rad
     else:
         return IReal(rad)
+    
 
 def exp(x):
     return IReal(math.e**x.inf,math.e**x.sup)
@@ -89,21 +42,26 @@ def sqrt(x):
         return IReal(x**(1.0/2.0))
 
 def abs(x):
-    return IReal(abs(x.inf),abs(x.sup))
+    if(type(x) is IReal):
+        return IReal(+abs(x.inf),+abs(x.sup))
+    else:
+        return np.abs(x)
 
 def sin(value):
     r = 0
-    r = (1 - (cos(value))**2)**(1.0/2.0)
-    return r
-
+    r = (IReal((-cos(value)).inf**2,(-cos(value)).sup**2) + 1)
+    r_1 = IReal(r.inf**(1.0/2.0),r.sup**(1.0/2.0))
+    return r_1
 
 def cos(rad):
     n = degToRad(rad)
     c = 0
-    r = 1
+    r = IReal(1)
     while(c < 50):
         c += 1
-        r += (((-1)**c)*(n**(2 * c)))/(math.factorial(2 * c))
+        t2 = IReal(n.inf**(2 * c),n.sup**(2 * c))
+        t3 = IReal(t2.inf/(math.factorial(2 * c)),t2.sup/(math.factorial(2 * c)))
+        r = r + IReal(((-1)**c)*t3.inf,((-1)**c)*t3.sup)
     return r
 
 #...............................................................................
@@ -111,10 +69,13 @@ def ackley( x, a=20, b=0.2, c=2*math.pi ):
     #x = np.asarray_chkfinite(x)  # ValueError if any NaN or Inf
     #print (x)
     n = len(x)
-    s1 = sum( [v**2 for v in x])
-    s2 = sum( [cos( c * v ) for v in x])
-    
-    return -a*exp( -b*( s1 / n )**(1.0/2.0)) - exp( s2 / n ) + a + exp(1)
+    s1 = sum( [IReal(v.inf**2,v.sup**2) for v in x])
+    s2 = sum( [cos( v * c ) for v in x])
+    t1_1 = IReal(s1.inf / n,s1.sup / n)
+    t1_2 = IReal(s2.inf / n,s2.sup / n)
+    t1_3 = IReal(t1_1.inf**(1.0/2.0),t1_1.sup**(1.0/2.0))
+    t2_1 = IReal(b*( t1_3.inf),b*( t1_3.sup))
+    return -IReal(a)*exp( -t2_1) - exp( t1_2 ) + IReal(a) + exp(IReal(1))
 
 #...............................................................................
 def dixonprice( x ):  # dp.m
@@ -122,14 +83,17 @@ def dixonprice( x ):  # dp.m
     n = len(x)
     j = np.arange( 2, n+1 )
     x2 = x**2 * 2
-    return sum( j * (x2[1:] - x[:-1]) **2 ) + (x[0] - 1) **2
+    t2 = IReal(0.0)
+    for i in range(len(x)-1):
+        t2+= x2[i+1] - x[i]
+    return sum( IReal(t2.inf**2,t2.sup**2)* j ) + (IReal((x[0].inf - 1)**2,(x[0].sup - 1)**2))
 
 #...............................................................................
 def griewank( x, fr=4000 ):
     #x = np.asarray_chkfinite(x)
     n = len(x)
     j = np.arange( 1., n+1 )
-    s = sum( [v**2 for v in x] )
+    s = sum( [IReal(v.inf**2,v.sup**2) for v in x] )
     v = []
     for i in range(len(x)):
         v.append(x[i]/sqrt(j[i]))
@@ -137,10 +101,12 @@ def griewank( x, fr=4000 ):
     return s/fr - p + 1
 
 #...............................................................................
+'''
 def levy( x ):
     x = np.asarray_chkfinite(x)
     n = len(x)
-    z = 1 + (x - 1) / 4.0
+    z_1 = (x - 1) + 1
+    z = IReal(z_1.inf/4.0,z_1.sup/4.0)
     t1 = sin( pi * z[0] )**2
     t2_1 = [(v - 1)**2 for v in x]
     t2_1 = t2_1[:-1]
@@ -151,7 +117,7 @@ def levy( x ):
     t2 = sum(t2_3)
     t3 = (z[-1] - 1)**2 * (1 + sin( 2 * math.pi * z[-1] )**2 )
     return t1 + t2 + t3      
-
+'''
 #...............................................................................
 michalewicz_m = .5  # orig 10: ^20 => underflow
 
@@ -160,23 +126,23 @@ def michalewicz( x ):  # mich.m
     n = len(x)
     j = np.arange( 1., n+1 )
     t1 = [sin(v) for v in x]
-    t2 = [sin( j[i] * x[i]**2 / math.pi ) for i in range(len(x))]
-    t3 = [t1[i] * t2[i]**((2 * michalewicz_m)) for i in range(len(t1))]
+    t2 = [sin(IReal(x[i].inf**2/ math.pi,x[i].sup**2/ math.pi ) * j[i]) for i in range(len(x))]
+    t3 = [IReal(t2[i].inf**((2 * michalewicz_m)),t2[i].sup**((2 * michalewicz_m)))*t1[i]  for i in range(len(t1))]
 
     return - sum(t3)
 
 #...............................................................................
+'''
 def perm( x, b=.5 ):
     x = np.asarray_chkfinite(x)
     n = len(x)
     j = np.arange( 1., n+1 )
-    xbyj = xbyj = [((km.abs(x[i])) / j[i]) for i in range(len(x))]
-    return mean([ mean( (j**k + b) * (xbyj ** k - 1) ) **2
-            for k in j/n ])
+    xbyj =  [((IReal(abs(x[i]).inf / j[i],abs(x[i]).sup / j[i]))) for i in range(len(x))]
+    return media([ media( (j**k + b) * IReal((xbyj.inf ** k - 1) **2,(xbyj.sup ** k - 1)  **2)) for k in j/n ])
     # original overflows at n=100 --
     # return sum([ sum( (j**k + b) * ((x / j) ** k - 1) ) **2
     #       for k in j ])
-
+'''
 #...............................................................................
 def powell( x ):
     x = np.asarray_chkfinite(x)
@@ -186,7 +152,7 @@ def powell( x ):
         x = np.append( x, np.zeros( n4 - n ))
     x = x.reshape(( 4, -1 ))  # 4 rows: x[4i-3] [4i-2] [4i-1] [4i]
     f = np.empty_like( x )
-    f[0] = x[0] + 10 * x[1]
+    f[0] = x[0] + x[1] * 10
     f[1] = sqrt(5) * (x[2] - x[3])
     f[2] = (x[1] - 2 * x[2]) **2
     f[3] = sqrt(10) * (x[0] - x[3]) **2
@@ -196,33 +162,42 @@ def powell( x ):
 def powersum( x, b=[8,18,44,114] ):  # power.m
     x = np.asarray_chkfinite(x)
     n = len(x)
-    s = 0
+    s = IReal(0)
+    
     for k in range( 1, n+1 ):
+        s_1 = IReal(0.0)
         bk = b[ min( k - 1, len(b) - 1 )]  # ?
-        s += (sum( x**k ) - bk) **2  # dim 10 huge, 100 overflows
+        for x_1 in x:
+            s_1 += (sum( IReal(x_1.inf**k,x_1.inf**k) ) - bk)   # dim 10 huge, 100 overflows
+        s+= IReal(s_1.inf**2,s_1.sup**2)
     return s
 
 #...............................................................................
 def rastrigin( x ):  # rast.m
     x = np.asarray_chkfinite(x)
     n = len(x)
-    return 10*n + sum([ v**2 - 10 * cos( 2 * math.pi * v )  for v in x  ])
+    return IReal(10*n) + sum([ IReal(v.inf**2,v.sup**2) - cos( v * 2 * math.pi ) * 10 for v in x  ])
 
 #...............................................................................
+'''
 def rosenbrock( x ):  # rosen.m
     """ http://en.wikipedia.org/wiki/Rosenbrock_function """
         # a sum of squares, so LevMar (scipy.optimize.leastsq) is pretty good
     x = np.asarray_chkfinite(x)
     x0 = x[:-1]
     x1 = x[1:]
-    return (sum( (1 - x0) **2 )
-        + 100 * sum( (x1 - x0**2) **2 ))
+    r = IReal(0.0)
+    for k in range (len(x0)):
+        t1 = sum( (x1[k] - IReal(x0[k].inf**2,x0[k].sup**2)) **2 )
+       r+= (sum( IReal((-x0[k].inf + 1) **2,(-x0[k].sup + 1) **2) + IReal(100) * t1 ))
+    
+'''
 
 #...............................................................................
 def schwefel( x ):  # schw.m
     x = np.asarray_chkfinite(x)
     n = len(x)
-    return 418.9829*n - sum( [v * sin( km.sqrt( km.abs( v ))) for v in x] )
+    return IReal(418.9829*n) - sum( [v * sin( sqrt( abs( v ))) for v in x] )
 
 #...............................................................................
 def sphere( x ):
@@ -234,7 +209,8 @@ def sum2( x ):
     x = np.asarray_chkfinite(x)
     n = len(x)
     j = np.arange( 1., n+1 )
-    return sum( j * x**2 )
+    x_1 = [IReal(v.inf**2,v.sup**2) for v in x]
+    return sum( x_1 * j )
 
 #...............................................................................
 def trid( x ):
@@ -246,24 +222,32 @@ def zakharov( x ):  # zakh.m
     x = np.asarray_chkfinite(x)
     n = len(x)
     j = np.arange( 1., n+1 )
-    s2 = sum( j * x ) / 2
+    s2_1 = sum( [IReal(v.inf * j,v.sup * j ) for v in x])
+    s2 = IReal(s2_1.inf/ 2,s2_1.sup/ 2)
     return sum( x**2 ) + s2**2 + s2**4
 
 #...............................................................................
     # not in Hedar --
-
+def media(x):
+    t = sum(x)
+    return IReal(t.inf/len(x),t.sup/len(x))
+    
 def ellipse( x ):
     x = np.asarray_chkfinite(x)
-    return mean( (1 - x) **2 )  + 100 * mean( np.diff(x) **2 )
-
+    t2 = np.diff(x)
+    return media( (-x + 1) **2 )  + media( t2 **2 ) * 100
 #...............................................................................
+
+def m(x):
+    return (x.inf+x.sup)/2.0
+
 def nesterov( x ):
     """ Nesterov's nonsmooth Chebyshev-Rosenbrock function, Overton 2011 variant 2 """
     x = np.asarray_chkfinite(x)
     x0 = x[:-1]
     x1 = x[1:]
-    t1 = abs( 1 - qm.centerI(x[0]) ) / 4
-    t2 = sum( [abs(qm.centerI( x1[i] - 2*abs(qm.centerI(x0[i])) + 1 )) for i in range(len(x1)) ])
+    t1 = abs( 1 - m(x[0]) ) / 4
+    t2 = sum( [abs(m( x1[i] - 2*abs(m(x0[i])) + 1 )) for i in range(len(x1)) ])
     return t1 + t2
 
 #...............................................................................
@@ -278,22 +262,22 @@ allfuncs = [
     ackley,
     dixonprice,
     ellipse,
-    griewank,
-    levy,
+ #   griewank,
+  #  levy,
     michalewicz,  # min < 0
     nesterov,
-    perm,
-    powell,
+  #  perm,
+  #  powell,
     # powellsincos,  # many local mins
     powersum,
     rastrigin,
-    rosenbrock,
+#    rosenbrock,
     schwefel,  # many local mins
     sphere,
     #saddle,
     sum2,
     trid,  # min < 0
-    zakharov,
+ #   zakharov,
     ]
 
 '''
@@ -325,18 +309,18 @@ name_to_func = { f.__name__ : f  for f in allfuncs }
 ackley._bounds       = [-15, 30]
 dixonprice._bounds   = [-10, 10]
 griewank._bounds     = [-600, 600]
-levy._bounds         = [-10, 10]
+#levy._bounds         = [-10, 10]
 michalewicz._bounds  = [0, pi]
-perm._bounds         = ["-dim", "dim"]  # min at [1 2 .. n]
-powell._bounds       = [-4, 5]  # min at tile [3 -1 0 1]
+#perm._bounds         = ["-dim", "dim"]  # min at [1 2 .. n]
+#powell._bounds       = [-4, 5]  # min at tile [3 -1 0 1]
 powersum._bounds     = [0, "dim"]  # 4d min at [1 2 3 4]
 rastrigin._bounds    = [-5.12, 5.12]
-rosenbrock._bounds   = [-2.4, 2.4]  # wikipedia
+#rosenbrock._bounds   = [-2.4, 2.4]  # wikipedia
 schwefel._bounds     = [-500.0, 500.0]
 sphere._bounds       = [-5.12, 5.12]
 sum2._bounds         = [-10, 10]
 trid._bounds         = ["-dim**2", "dim**2"]  # fmin -50 6d, -200 10d
-zakharov._bounds     = [-5, 10]
+#zakharov._bounds     = [-5, 10]
 
 ellipse._bounds      =  [-2, 2]
 #logsumexp._bounds    = [-20, 20]  # ?
@@ -391,7 +375,7 @@ def makeList(steps,dim):
     r = []
     for k in steps:
         for i in range(dim):
-            r.append(Kaucher(k))
+            r.append(IReal(k))
         result.append(r)
         r = []
 
